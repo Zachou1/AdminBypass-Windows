3ca9dd546c7fa71c9b358167b7ebcec0cbe118ab
NoOneWillEverKnowMyName <77148048+Zachou1@users.noreply.github.com>
2025-12-03 20:31:11 +0100

    Create execute.bat for RunAsInvoker functionality
    Add a batch script to run a file as invoker.

---
+++ RunFiles/RunAsInvoker/execute.bat
@@ -0,0 +1,3 @@
+Set _COMPACT_LAYER=RunAsInvoker
+Start file_name.extension & ::Full name ( include .extension | ex : file.exe )
+::The file should be in the same directory as this invoker script. If that is not the case, write the path to the file.
